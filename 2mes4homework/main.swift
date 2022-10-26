a8cac7eb0fdf9251ae72842d63afed61e2a16e70
safarliev <nurikonurik@gmail.com>
2022-10-26 16:41:21 +0600

    ...

--- 2mes4homework/main.swift
+++ 2mes4homework/main.swift
@@ -7,5 +7,15 @@
 
 import Foundation
 
-print("Hello, World!")
+var milk = Product(name: "Молоко", price: 60, amount: 1)
+var pepsi = Product(name: "Pepsi", price: 55, amount: 2)
+var bread = Product(name: "Хлеб", price: 20, amount: 2)
 
+var pokupatel1 = Pokupatel(name: "Nurik", nalichka: 500)
+
+pokupatel1.addProduct(product: milk)
+pokupatel1.addProduct(product: pepsi)
+pokupatel1.addProduct(product: bread)
+
+pokupatel1.cheklist()
+pokupatel1.sdacha()
